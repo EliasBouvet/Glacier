58e62c06dff364fa86e3671867748ece93c6fb0d
Asbjørn Rørvik <rorvik.asbjorn@gmail.com>
2021-08-13 10:23:44 +0200

    Oppdatert dashbord

--- glacier/src/views/Dashboard/Dashboard.js
+++ glacier/src/views/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import { firestore } from "Firebase/firebaseConfig"; //"./firebaseConfig";
 // react plugin for creating charts
 import ChartistGraph from "react-chartist";
 // react plugin for creating vector maps
@@ -76,6 +77,211 @@ var mapData = {
 const useStyles = makeStyles(styles);
 
 export default function Dashboard() {
+
+
+///FIREBASE
+
+  ///FIREBASE
+  const [powerdata, setpowerdata] = useState([]);
+  const [powerdata2, setpowerdata2] = useState([]);
+  const [powerdata3, setpowerdata3] = useState([]);
+  const [powerdata4, setpowerdata4] = useState([]);
+  const [powerdata5, setpowerdata5] = useState([]);
+  
+  // Initial load, get data
+  /*
+  useEffect(() => {
+    getFirebaseData();
+  
+  
+    
+  }, []);
+  */
+  const chartData = {
+    labels:[],
+    series:[[]]
+  };
+  
+  const chartData2 = { labels:[],series:[[]]};
+  const chartData3 = { labels:[],series:[[]]};
+  const chartData4 = { labels:[],series:[[]]};
+  const chartData5 = { labels:[],series:[[]]};
+  
+   // const seriesData = [];
+  
+    var options2 = {
+      high: 1000,
+      low: 0,
+      axisX: {
+        labelInterpolationFnc: function(value, index) {
+          return index % 2 === 0 ? value : null;
+        }
+      }
+    };
+  
+    var options3 = {
+      high: 60,
+      low: 0,
+      axisX: {
+        labelInterpolationFnc: function(value, index) {
+          return index % 2 === 0 ? value : null;
+        }
+      }
+    };
+  
+    var options4 = {
+      high: 400,
+      low: 0,
+      axisX: {
+        labelInterpolationFnc: function(value, index) {
+          return index % 2 === 0 ? value : null;
+        }
+      }
+    };
+  
+    var options5 = {
+      high: 10,
+      low: 0,
+      axisX: {
+        labelInterpolationFnc: function(value, index) {
+          return index % 2 === 0 ? value : null;
+        }
+      }
+    };
+  
+    var options6 = {
+      high: 10,
+      low: 0,
+      axisX: {
+        labelInterpolationFnc: function(value, index) {
+          return index % 2 === 0 ? value : null;
+        }
+      }
+    };
+  
+  const getFirebaseData = async () => {
+    const oneDayBefore = getOneDayBeforeDate();
+    
+    // Main statement
+    const firebaseData = await firestore
+    .collection("powerprice")
+    .where("timestamp", ">=", oneDayBefore)
+    .get();
+  
+    const firebaseData2 = await firestore
+    .collection("waterLevel")
+    .where("timestamp", ">=", oneDayBefore)
+    .get();
+  
+    const firebaseData3 = await firestore
+    .collection("waterInflux")
+    .where("timestamp", ">=", oneDayBefore)
+    .get();
+  
+    const firebaseData4 = await firestore
+    .collection("money")
+    .where("timestamp", ">=", oneDayBefore)
+    .get();
+  
+    const firebaseData5 = await firestore
+    .collection("environmentCost")
+    .where("timestamp", ">=", oneDayBefore)
+    .get();
+    
+    
+  
+    
+    const d = firebaseData.docs.map((doc) => {
+      // doc.data() returns {powerPrice, timestamp}
+      return doc.data();
+    });
+  
+    const d2 = firebaseData2.docs.map((doc) => {
+      // doc.data() returns {powerPrice, timestamp}
+      return doc.data();
+    });
+  
+    const d3 = firebaseData3.docs.map((doc) => {
+      // doc.data() returns {powerPrice, timestamp}
+      return doc.data();
+    });
+    const d4 = firebaseData4.docs.map((doc) => {
+      // doc.data() returns {powerPrice, timestamp}
+      return doc.data();
+    });
+  
+    const d5 = firebaseData5.docs.map((doc) => {
+      // doc.data() returns {powerPrice, timestamp}
+      return doc.data();
+    });
+      
+    
+    
+  //  Number("1000")
+    
+    d.forEach(chartD=>{
+      chartData.labels.push(chartD.timestamp.toDate().getHours().toString());
+      chartData.series.push(Math.floor(chartD.powerPrice));
+    })
+  
+    d2.forEach(chartD=>{
+      chartData2.labels.push(chartD.timestamp.toDate().getHours().toString());
+      chartData2.series.push(Math.floor(chartD.waterLevel));
+    })
+  
+    d3.forEach(chartD=>{
+      chartData3.labels.push(chartD.timestamp.toDate().getHours().toString());
+      chartData3.series.push(Math.floor(chartD.waterInflux));
+    })
+  
+    d4.forEach(chartD=>{
+      chartData4.labels.push(chartD.timestamp.toDate().getHours().toString());
+      chartData4.series.push(Math.floor(chartD.money)/100000000);
+    })
+  
+    d5.forEach(chartD=>{
+      chartData5.labels.push(chartD.timestamp.toDate().getHours().toString());
+      chartData5.series.push(Math.floor(chartD.environmentCost)/10000000);
+    })
+    
+    //chartData.series.concat(seriesData);
+    chartData.series = [chartData.series];
+    chartData.series[0].shift();
+  
+    chartData2.series = [chartData2.series];
+    chartData2.series[0].shift();
+  
+    chartData3.series = [chartData3.series];
+    chartData3.series[0].shift();
+  
+    chartData4.series = [chartData4.series];
+    chartData4.series[0].shift();
+  
+    chartData5.series = [chartData5.series];
+    chartData5.series[0].shift();
+  
+    console.log(chartData);
+    console.log(chartData2);
+    console.log(chartData3);
+    console.log(chartData4);
+    console.log(chartData5);
+    //update state
+    setpowerdata(chartData);
+    setpowerdata2(chartData2);
+    setpowerdata3(chartData3);
+    setpowerdata4(chartData4);
+    setpowerdata5(chartData5);
+    };
+    
+  // Helper function to get 24 hrs before day
+  const getOneDayBeforeDate = () => {
+  return new Date(new Date().getTime() - 1 * 60 * 60 * 1000);
+  };
+  
+
+///FIREBASE AVSLUTT
+
+
   const [waterLevel, setWaterLevel] = useState(0)
   const [powerPrice, setPowerPrice] = useState(0)
   const [waterInflux, setWaterInflux] = useState(0)
@@ -95,9 +301,9 @@ export default function Dashboard() {
       return res.json()
    })
    .then((data) =>{
-     const formatMoneyEarned = new Intl.NumberFormat('no-NO', { style: 'currency', currency: 'NOK' }).format(data.money)
+     const formatMoneyEarned = new Intl.NumberFormat('no-NO', { style: 'currency', currency: 'NOK' }).format(data.money/1000000)
      setMoneyEarned(formatMoneyEarned)
-     const formatEnvmCost = new Intl.NumberFormat('no-NO', { style: 'currency', currency: 'NOK' }).format(data.environmentCost)
+     const formatEnvmCost = new Intl.NumberFormat('no-NO', { style: 'currency', currency: 'NOK' }).format(data.environmentCost/1000000)
      setEnvironmentCost(formatEnvmCost)
      let waterLevelRounded = Math.round(data.waterLevel)
      if (data.waterLevel > 40) {
@@ -141,6 +347,7 @@ export default function Dashboard() {
 
   useEffect(() => {
     updateData()
+    getFirebaseData();
   }, [])
 
   return (
@@ -172,27 +379,32 @@ export default function Dashboard() {
         <GridItem xs={12} sm={12} md={6} lg={6}>
           <Card>
             <CardHeader color="info" stats icon>
-              <p className={classes.cardCategory}>Værprognose</p>
+            <CardIcon color="info">
+                <Icon>umbrella</Icon>
+              </CardIcon>
+              <p className={classes.cardCategory}>Værprognose for Bodø</p>
               <h3 className={classes.cardTitle}>
-                Forventet nedbør i Bodø neste 7 dager: 15 mm
+                15 mm
               </h3>
             </CardHeader>
             <CardFooter stats>
               <div className={classes.stats}>
                 <DateRange />
-                Værmelding skal hentes fra yr
+                <p>
+                Værmelding hentes fra yr
+                </p>
               </div>
             </CardFooter>
           </Card>
         </GridItem>
         <GridItem xs={12} sm={6} md={6} lg={3}>
           <Card>
-            <CardHeader color="info" stats icon>
-              <CardIcon color="info">
+            <CardHeader color="warning" stats icon>
+              <CardIcon color="warning">
                 <Icon>paid</Icon>
               </CardIcon>
               <p className={classes.cardCategory}>Strømpris</p>
-              <h3 className={classes.cardTitle}>{powerPrice} kr/MWh</h3>
+              <h3 className={classes.cardTitle}>kr {powerPrice}/MWh</h3>
             </CardHeader>
             <CardFooter stats>
               <div className={classes.stats}>
@@ -209,7 +421,7 @@ export default function Dashboard() {
                 <Icon>savings</Icon>
               </CardIcon>
               <p className={classes.cardCategory}>Penger tjent</p>
-              <h3 className={classes.cardTitle}>{moneyEarned} kr</h3>
+              <h3 className={classes.cardTitle}>{moneyEarned}M</h3>
             </CardHeader>
             <CardFooter stats>
               <div className={classes.stats}>
@@ -238,12 +450,12 @@ export default function Dashboard() {
         </GridItem>
         <GridItem xs={12} sm={6} md={6} lg={3}>
           <Card>
-            <CardHeader color="info" stats icon>
+            <CardHeader color="danger" stats icon>
               <CardIcon color="danger">
                 <Icon>report_problem</Icon>
               </CardIcon>
               <p className={classes.cardCategory}>Miljøkostnad</p>
-              <h3 className={classes.cardTitle}>{environmentCost} kr</h3>
+              <h3 className={classes.cardTitle}>{environmentCost}M</h3>
             </CardHeader>
             <CardFooter stats>
               <div className={classes.stats}>
@@ -255,6 +467,131 @@ export default function Dashboard() {
         </GridItem>
       </GridContainer>
       <GridContainer>
+        <GridItem xs={12} sm={6} md={6} lg={4}>
+        <Card chart className={classes.cardHover}>
+            <CardHeader color="info" className={classes.cardHeaderHover}>
+              <ChartistGraph
+                className="ct-chart-white-colors"
+                data={powerdata2}
+                type="Line"
+                options={options3}
+                listener={dailySalesChart.animation}
+              />
+            </CardHeader>
+            <CardBody>
+              <div className={classes.cardHoverUnder}>
+                <Tooltip
+                  id="tooltip-top"
+                  title="Refresh"
+                  placement="bottom"
+                  classes={{ tooltip: classes.tooltip }}
+                >
+                  <Button simple color="info" justIcon>
+                    <Refresh className={classes.underChartIcons} />
+                  </Button>
+                </Tooltip>
+                <Tooltip
+                  id="tooltip-top"
+                  title="Change Date"
+                  placement="bottom"
+                  classes={{ tooltip: classes.tooltip }}
+                >
+                  <Button color="transparent" simple justIcon>
+                    <Edit className={classes.underChartIcons} />
+                  </Button>
+                </Tooltip>
+              </div>
+              <h4 className={classes.cardTitle}>Vann Nivå</h4>
+              <p className={classes.cardCategory}>
+                Siste 1 time
+              </p>
+            </CardBody>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={6} lg={4}>
+        <Card chart className={classes.cardHover}>
+            <CardHeader color="warning" className={classes.cardHeaderHover}>
+              <ChartistGraph
+                className="ct-chart-white-colors"
+                data={powerdata}
+                type="Line"
+                options={options2}
+                listener={dailySalesChart.animation}
+              />
+            </CardHeader>
+            <CardBody>
+              <div className={classes.cardHoverUnder}>
+                <Tooltip
+                  id="tooltip-top"
+                  title="Refresh"
+                  placement="bottom"
+                  classes={{ tooltip: classes.tooltip }}
+                >
+                  <Button simple color="info" justIcon>
+                    <Refresh className={classes.underChartIcons} />
+                  </Button>
+                </Tooltip>
+                <Tooltip
+                  id="tooltip-top"
+                  title="Change Date"
+                  placement="bottom"
+                  classes={{ tooltip: classes.tooltip }}
+                >
+                  <Button color="transparent" simple justIcon>
+                    <Edit className={classes.underChartIcons} />
+                  </Button>
+                </Tooltip>
+              </div>
+              <h4 className={classes.cardTitle}>Strøm pris</h4>
+              <p className={classes.cardCategory}>
+                Siste time
+              </p>
+            </CardBody>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={6} lg={4}>
+        <Card chart className={classes.cardHover}>
+            <CardHeader color="danger" className={classes.cardHeaderHover}>
+              <ChartistGraph
+                className="ct-chart-white-colors"
+                data={powerdata5}
+                type="Line"
+                options={options6}
+                listener={dailySalesChart.animation}
+              />
+            </CardHeader>
+            <CardBody>
+              <div className={classes.cardHoverUnder}>
+                <Tooltip
+                  id="tooltip-top"
+                  title="Refresh"
+                  placement="bottom"
+                  classes={{ tooltip: classes.tooltip }}
+                >
+                  <Button simple color="info" justIcon>
+                    <Refresh className={classes.underChartIcons} />
+                  </Button>
+                </Tooltip>
+                <Tooltip
+                  id="tooltip-top"
+                  title="Change Date"
+                  placement="bottom"
+                  classes={{ tooltip: classes.tooltip }}
+                >
+                  <Button color="transparent" simple justIcon>
+                    <Edit className={classes.underChartIcons} />
+                  </Button>
+                </Tooltip>
+              </div>
+              <h4 className={classes.cardTitle}>Miljøkostnad</h4>
+              <p className={classes.cardCategory}>
+                Siste time
+              </p>
+            </CardBody>
+          </Card>
+        </GridItem>
+      </GridContainer>
+      <GridContainer>
         <GridItem xs={12}>
           <Card>
             <CardHeader color="success" icon>
@@ -262,17 +599,17 @@ export default function Dashboard() {
                 <Language />
               </CardIcon>
               <h4 className={classes.cardIconTitle}>
-                Strøm produsert de sist 24 timene
+                Inntekt siste time
               </h4>
             </CardHeader>
             <CardBody>
             <Card chart>
-            <CardHeader color="warning">
+            <CardHeader color="success">
               <ChartistGraph
                 className="ct-chart-white-colors"
-                data={straightLinesChart.data}
+                data={powerdata4}
                 type="Line"
-                options={straightLinesChart.options}
+                options={options5}
                 listener={straightLinesChart.animation}
               />
             </CardHeader>
